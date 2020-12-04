a40d58418598405bd5b3a0637332174076d0aa88
dtangneuro <72279273+dtangneuro@users.noreply.github.com>
2020-12-04 17:03:13 -0600

    Initial Commit
    Pseudocode to identify high sensitivity units

---
+++ KNNClassifierHighSensitivityUnits.m
@@ -0,0 +1,10 @@
+%% IDENTIFY HIGHLY SENSITIVE UNITS
+% load unit activity and lingual-palatal contact(lpc)
+% load or run spike calculations for each window of set duration
+% create 2-D matrix of variation for each unit and trial
+    % this will be a hint as to which units are the most sensitive
+% run cross correlation between binary contact and spikes/window to check
+% for lag
+% Do the same for graded gape cycle and spikes/window
+% Take correlation(0-1) for each unit for each trial and run basic stats to
+% identify most sensitive and how much more sensitive
